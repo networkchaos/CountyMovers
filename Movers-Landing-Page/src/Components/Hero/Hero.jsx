0ae75bb7cf311f60888409bbe87920f2308d23a2
theMungai <mungaithesimon@gmail.com>
2025-01-21 22:46:37 +0300

    Hero Background and Structure

---
+++ Movers-Landing-Page/src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-text'>
+        <h1>Your Trusted Moving </h1>
+        <h1>Partner</h1>
+        <p>Professional,reliable, and stress-free moving services across the country</p>
+        <button>Get a Free Quote</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
