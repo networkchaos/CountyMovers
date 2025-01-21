23a6a25c3f1b7feaefce0e03ace32e791d76be93
theMungai <mungaithesimon@gmail.com>
2025-01-21 21:28:54 +0300

    Navbar Structure

--- Movers-Landing-Page/src/App.jsx
+++ Movers-Landing-Page/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar></Navbar>
     </div>
   )
 }
---
+++ Movers-Landing-Page/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css' 
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>CountyMovers</h1>
+      <ul>
+        <li>Services</li>
+        <li>About</li>
+        <li>Pricing</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
