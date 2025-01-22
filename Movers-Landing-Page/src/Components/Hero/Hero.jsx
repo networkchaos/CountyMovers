32b0c2640a783f3462be57c9c0e70fbb89092ab7
theMungai <mungaithesimon@gmail.com>
2025-01-22 09:06:15 +0300

    Hero Button

--- Movers-Landing-Page/src/Components/Hero/Hero.jsx
+++ Movers-Landing-Page/src/Components/Hero/Hero.jsx
@@ -8,7 +8,14 @@ const Hero = () => {
         <h1>Your Trusted Moving </h1>
         <h1>Partner</h1>
         <p>Professional,reliable, and stress-free moving services across the country</p>
-        <button>Get a Free Quote</button>
+        <button>
+          Get a Free Quote
+          <svg width="37" height="16" viewBox="0 0 37 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M36 8L1 8" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M29 15L36 8L29 0.999999" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+          </button>
       </div>
     </div>
   )
