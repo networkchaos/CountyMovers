b4cb76e8c654683aa5c1f28f513d766f3c664d03
theMungai <mungaithesimon@gmail.com>
2025-01-22 11:36:18 +0300

    Our Services Section

--- Movers-Landing-Page/src/App.jsx
+++ Movers-Landing-Page/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Services from './Components/Services/Services'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Services></Services>
     </div>
   )
 }
