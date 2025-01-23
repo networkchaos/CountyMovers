09a1bf52c59e679bd96b901dfaa951eeb5d6cb68
theMungai <mungaithesimon@gmail.com>
2025-01-23 22:15:56 +0300

    Fixed side navigation menu

--- Movers-Landing-Page/src/Components/Navbar/Navbar.jsx
+++ Movers-Landing-Page/src/Components/Navbar/Navbar.jsx
@@ -13,18 +13,21 @@ const Navbar = () => {
     })
   },[])
 
-  
+  const [mobileMenu, setMobileMenu] = useState(false)
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+  }
 
   return (
     <nav className={`nav-container ${sticky ? "dark-nav" : ""}`}>
       <h1><Link to='hero' smooth={true}duration={800}>CountyMovers</Link></h1>
-      <ul>
+      <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li><Link to='our-services' smooth={true}duration={800} offset={-150}>Services</Link></li>
         <li><Link to='about-container' smooth={true}duration={800} offset={-150}>About</Link></li>
         <li><Link to='our-pricing-container' smooth={true}duration={800} offset={-150}>Pricing</Link></li>
         <li><Link to='contact-container' smooth={true}duration={800} offset={-150}>Contact</Link></li>
       </ul>
-      <img src={menu_icon} alt="" className='menu-icon' />
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
     </nav>
   )
 }
